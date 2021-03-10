8d6579d10f91ab850a70ba9b0dde4f6c00ad8337
pedroleaop <pedroleaop@hotmail.com>
2021-03-10 09:45:40 -0300

    api and first component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke,index) in pokemons" :key="index">
+        <pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import pokemon from './components/pokemon.vue';
 export default {
-  name: 'App'
-}
+  components: { pokemon },
+  name: 'App',
+  data(){
+    return {
+        pokemons: []
+    }
+  },
+ created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(response => (
+      this.pokemons= response.data.results))
+}}
 </script>
 
 <style>
---
+++ src/components/pokemon.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+  <h1>{{num}} {{name | upper}}</h1>
+  <small>{{url}}</small>
+
+        <div class="card">
+        <div class="card-image">
+            <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">                </figure>
+            </div>
+            <div class="media-content">
+                <p class="title is-4">John Smith</p>
+                <p class="subtitle is-6">@johnsmith</p>
+            </div>
+            </div>
+            <div class="content">
+            </div>
+        </div>
+        </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            pokemon: {}
+        }
+    },
+    created: function(){
+        axios.get(this.url).then(res => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            console.log(this.pokemon);
+        })
+    },
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    filters: {
+        upper: function(value) {
+            var NewName = value[0].toUpperCase() + value.slice(1);
+            return NewName;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
